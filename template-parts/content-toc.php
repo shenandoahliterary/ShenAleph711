9e79d151c5e491cdb554116f5fc4a8c39bf78c02
Jeff Barry <10522393+jeffreybarry@users.noreply.github.com>
2021-12-05 13:15:43 -0500

    Update content-toc.php
    test of outside loop did not work

--- template-parts/content-toc.php
+++ template-parts/content-toc.php
@@ -109,16 +109,6 @@ URLLINK;
 }
 					$i++;
 				endwhile;
-				//check for author's note
-$custom_fields = get_post_custom();
-$has_author_note = $custom_fields['has_author_note'];
-if (! empty($has_author_note)) {
-	$author_note_url = site_url(); 
-//	echo "Author's Note $author_note_url/$has_author_note[$i]/ <br />";
-echo <<<URLLINK
-<a href="$author_note_url/$has_author_note[$i]/">Author's Note</a><br />
-URLLINK;
-}
 
 
 				//print author outside of the loop
