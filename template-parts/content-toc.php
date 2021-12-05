161c2d06a857803b24a6418e4b9b1a385b7aec33
Jeff Barry <10522393+jeffreybarry@users.noreply.github.com>
2021-12-05 13:13:11 -0500

    Update content-toc.php
    fix incorrect error

--- template-parts/content-toc.php
+++ template-parts/content-toc.php
@@ -113,14 +113,6 @@ URLLINK;
 				
 
 
-				$has_author_note = $custom_fields['has_author_note'];
-				if (! empty($has_author_note)) {
-					$author_note_url = site_url(); 
-				//	echo "Author's Note $author_note_url/$has_author_note[$i]/ <br />";
-				echo <<<URLLINK
-				<a href="$author_note_url/$has_author_note[$i]/">Author's Note</a><br />
-				URLLINK;
-
 				//print author outside of the loop
 				?>
 				<span class="author_name"><?php the_author(); ?> </span>
