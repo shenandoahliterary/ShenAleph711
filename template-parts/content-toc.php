4cc4e36fa49d03b35fb081caf3ac685fd066a4f9
Jeff Barry <10522393+jeffreybarry@users.noreply.github.com>
2021-12-06 14:36:18 -0500

    Update content-toc.php

--- template-parts/content-toc.php
+++ template-parts/content-toc.php
@@ -103,7 +103,7 @@ $custom_fields = get_post_custom();
 $has_author_note = $custom_fields['has_author_note'];
 
 //echo "$author_note_url/$has_author_note[$i]/ <br />";
-/*
+
 if (! empty($has_author_note)) {
 	$author_note_url = site_url(); 
 //	echo "Author's Note $author_note_url/$has_author_note[$i]/ <br />";
@@ -111,7 +111,7 @@ if (! empty($has_author_note)) {
 echo <<<URLLINK
 <a href="$author_note_url/$has_author_note[$i]/">Author's Note</a><br />
 URLLINK;
-*/
+
 }
 
 					$i++;
